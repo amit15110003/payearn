7d56cae1bc217e48976d9ccea702cf5a86d4b87a
AMIT ANAND <amitanand.kvj.2@gmail.com>
2018-07-28 23:55:27 +0530

    sfdg

--- application/views/search.php
+++ application/views/search.php
@@ -3,24 +3,6 @@
 					<div class="container">
 						<div class="row">
 							<div class="col-md-12">
-								<div class="shop-filter">
-									<div class="col-md-6">
-										<p class="result-count"> </p>
-									</div>
-									<div class="col-md-6">
-										<div class="shop-filter-right">
-											<form class="commerce-ordering">
-												<select name="orderby" class="orderby">
-													<option value="">Sort by popularity</option>
-													<option value="">Sort by average rating</option>
-													<option value="" selected="selected">Sort by newness</option>
-													<option value="">Sort by price: low to high</option>
-													<option value="">Sort by price: high to low</option>
-												</select>
-											</form>
-										</div>
-									</div>
-								</div>
 								<div class="product-grid">
 									<?php foreach ($query as $row) {
 				$p_category=str_replace(' ', '-', $row->p_category);
@@ -62,6 +44,25 @@
 									<span class="page-numbers current">2</span> 
 									<a class="page-numbers" href="#">3</a> 
 									<a class="next page-numbers" href="#">Next</a>-->
+								</div>
+							</div>
+							<div class="col-md-3 col-md-pull-9">
+								<div class="sidebar">
+									<div class="widget widget-product-search">
+										<form class="form-search">
+											<input type="text" class="search-field" placeholder="Search products…" value="" name="s" />
+											<input type="submit" value="Search" />
+										</form>
+									</div>
+									<div class="widget widget-product-categories">
+										<h3 class="widget-title">Product Categories</h3>
+										<ul class="product-categories">
+											<li><a href="#">Dried</a> <span class="count">6</span></li>
+											<li><a href="#">Fruits</a> <span class="count">5</span></li>
+											<li><a href="#">Juice</a> <span class="count">6</span></li>
+											<li><a href="#">Vegetables</a> <span class="count">6</span></li>
+										</ul>
+									</div>
 								</div>
 							</div>
 						</div>
