5df4997f9c44f3eb3571e0bfd483336d7114753f
AMIT ANAND <amitanand.kvj.2@gmail.com>
2018-07-31 00:49:05 +0530

    ds

--- application/views/index.php
+++ application/views/index.php
@@ -36,7 +36,11 @@
 											</a>
 											<div class="product-action">
 												<span class="add-to-cart">
-													<a href="#" data-toggle="tooltip" data-placement="top" title="Add to cart"></a>
+													<?php if(!empty($this->session->userdata('u_id'))){?>
+													<a onclick="javascript:cartadd(<?php echo $row->p_id;?>);" data-toggle="tooltip" data-placement="top" title="Add to cart"></a>
+			                					<?php }else{?>
+													<a onclick="javascript:cartadd1(<?php echo $row->p_id;?>);" data-toggle="tooltip" data-placement="top" title="Add to cart"></a>
+			                					<?php }?>
 												</span>
 												<span class="wishlist">
 													<?php if(!empty($this->user->check_wish($this->session->userdata('u_id'),$row->p_id))) {?>
